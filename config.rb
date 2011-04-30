6096ea831d3df32b1f39e35d1acdc8103e0d670d
Daniel Barlow <dan@telent.net>
2011-04-30 23:04:15 +0100

    switching to private git repo, just put everything right in there

---
+++ config.rb
@@ -0,0 +1,33 @@
+FlickRaw.api_key="7da74d0034b15043174132479f248d6c"
+FlickRaw.shared_secret= "ff673486ac83ab2d"
+
+module Adsense
+  Box_ad=
+    %q{
+<script type="text/javascript"><!--
+google_ad_client = "ca-pub-1724530698833659";
+/* sidebar small square */
+google_ad_slot = "6840898431";
+google_ad_width = 200;
+google_ad_height = 200;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+}
+  Thin_ad=
+    %q{
+<script type="text/javascript"><!--
+google_ad_client = "ca-pub-1724530698833659";
+/* thin */
+google_ad_slot = "0823300994";
+google_ad_width = 468;
+google_ad_height = 60;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+}
+end
